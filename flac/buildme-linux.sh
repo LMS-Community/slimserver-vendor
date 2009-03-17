cb4ed89979c7895b99c276ea4b354a8f255c7043
Andy Grundman <agrundman@slimdevices.com>
2009-03-17 21:45:29 +0000

    Build flac with libogg
    git-svn-id: file:///home/awy/mirror/slim/7.3/trunk/vendor@25590 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- flac/buildme-linux.sh
+++ flac/buildme-linux.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+OGG=1.1.3
 FLAC=1.2.1
 LOG=$PWD/config.log
 CHANGENO=` svn info .  | grep -i Revision | awk -F": " '{print $2}'`
@@ -14,6 +15,16 @@ rm -rf flac-$FLAC
 echo "Most log mesages sent to $LOG... only 'errors' displayed here"
 date > $LOG
 
+## Build Ogg first
+echo "Untarring libogg-$OGG.tar.gz..."
+tar -zxf libogg-$OGG.tar.gz
+cd libogg-$OGG
+echo "Configuring..."
+./configure --disable-shared >> $LOG
+echo "Running make..."
+make >> $LOG
+cd ..
+
 ## Build
 echo "Untarring..."
 tar zxvf flac-$FLAC.tar.gz >> $LOG
@@ -21,7 +32,7 @@ cd flac-$FLAC >> $LOG
 patch -p0 < ../sc.patch >> $LOG
 patch -p0 < ../triode-ignore-wav-length.patch >> $LOG
 echo "Configuring..."
-./configure --disable-doxygen-docs --disable-shared --disable-xmms-plugin --disable-cpplibs --disable-ogg --disable-oggtest --prefix $OUTPUT >> $LOG
+./configure --with-ogg-includes=$PWD/../libogg-$OGG/include --with-ogg-libraries=$PWD/../libogg-$OGG/src/.libs/ --disable-doxygen-docs --disable-shared --disable-xmms-plugin --disable-cpplibs --prefix $OUTPUT >> $LOG
 echo "Running make"
 make >> $LOG
 echo "Running make install"
@@ -32,3 +43,4 @@ cd ..
 tar -zcvf $OUTPUT.tgz $OUTPUT
 rm -rf $OUTPUT
 rm -rf flac-$FLAC
+rm -rf libogg-$OGG
