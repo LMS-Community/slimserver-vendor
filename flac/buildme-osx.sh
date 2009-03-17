3b20dc0e03e8c8af5742a3cf6da87daaf7d12713
Andy Grundman <agrundman@slimdevices.com>
2009-03-17 22:14:23 +0000

    r51917@macbookpro (orig r25586):  andy | 2009-03-17 11:41:44 -0400  Increase delay in remote scanner as a quick workaround for playlists that contain backup files, to more likely prefer the stream  r51931@macbookpro (orig r25588):  andy | 2009-03-17 17:03:30 -0400  Build scripts for custom flac build  r51933@macbookpro (orig r25590):  andy | 2009-03-17 17:45:29 -0400  Build flac with libogg  r51934@macbookpro (orig r25591):  andy | 2009-03-17 18:10:32 -0400  Bug 11223, new flac build with patch to allow for infinite streams
    git-svn-id: file:///home/awy/mirror/slim/7.4/trunk/vendor@25592 62299810-d8cb-41fd-93b7-d32162e5a4a4

---
+++ flac/buildme-osx.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+OGG=1.1.3
+FLAC=1.2.1
+LOG=$PWD/config.log
+CHANGENO=` svn info .  | grep -i Revision | awk -F": " '{print $2}'`
+ARCH="osx"
+OUTPUT=$PWD/flac-build-$ARCH-$CHANGENO
+
+# Mac Universal Binary support
+CFLAGS="-isysroot /Developer/SDKs/MacOSX10.4u.sdk -arch i386 -arch ppc -mmacosx-version-min=10.3"
+LDFLAGS="-arch i386 -arch ppc"
+
+# Clean up
+rm -rf $OUTPUT
+rm -rf flac-$FLAC
+
+## Start
+echo "Most log mesages sent to $LOG... only 'errors' displayed here"
+date > $LOG
+
+## Build Ogg first
+echo "Untarring libogg-$OGG.tar.gz..."
+tar -zxf libogg-$OGG.tar.gz
+cd libogg-$OGG
+echo "Configuring..."
+./configure CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" --disable-shared --disable-dependency-tracking >> $LOG
+echo "Running make..."
+make >> $LOG
+cd ..
+
+## Build
+echo "Untarring..."
+tar zxvf flac-$FLAC.tar.gz >> $LOG
+cd flac-$FLAC >> $LOG
+patch -p0 < ../sc.patch >> $LOG
+patch -p0 < ../triode-ignore-wav-length.patch >> $LOG
+echo "Configuring..."
+./configure CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" --with-ogg-includes=$PWD/../libogg-$OGG/include --with-ogg-libraries=$PWD/../libogg-$OGG/src/.libs/ --disable-doxygen-docs --disable-shared --disable-xmms-plugin --disable-dependency-tracking --disable-asm-optimizations --disable-cpplibs --prefix $OUTPUT >> $LOG
+echo "Running make"
+make >> $LOG
+echo "Running make install"
+make install >> $LOG
+cd ..
+
+## Tar the whole package up
+tar -zcvf $OUTPUT.tgz $OUTPUT
+rm -rf $OUTPUT
+rm -rf flac-$FLAC
+rm -rf libogg-$OGG
