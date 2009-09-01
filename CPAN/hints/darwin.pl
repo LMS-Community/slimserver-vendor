fadfeecd07c6d0df46b7e7dd367c953b64b8260e
Andy Grundman <agrundman@slimdevices.com>
2009-09-01 18:33:05 +0000

    r58349@macbookpro-eth (orig r28401):  andy | 2009-09-01 14:29:41 -0400  Don't build ppc support for 5.10 modules  r58350@macbookpro-eth (orig r28402):  andy | 2009-09-01 14:29:10 -0400  Rebuilt modules for Snow Leopard without ppc support to reduce disk space a bit
    git-svn-id: file:///home/awy/mirror/slim/7.4/trunk/vendor@28403 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- CPAN/hints/darwin.pl
+++ CPAN/hints/darwin.pl
@@ -5,7 +5,7 @@ use Config;
 if ( $Config{myarchname} =~ /i386/ ) {
     if ( $Config{version} =~ /^5\.10/ ) {
         # 5.10, build as 10.5+ with Snow Leopard 64-bit support
-        $arch = "-arch x86_64 -arch i386 -arch ppc -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5";
+        $arch = "-arch x86_64 -arch i386 -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5";
     }
     else {
         # 5.8.x, build for 10.3+ 32-bit universal
