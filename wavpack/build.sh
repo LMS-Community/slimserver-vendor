95664b5d1e2107c038f0d5742d847c8b75591d01
Andy Grundman <agrundman@slimdevices.com>
2009-10-16 22:05:52 +0000

    r59585@macbookpro (orig r28890):  michael | 2009-10-16 09:38:04 -0400  Bug: n/a  Description: remove build detail from OS name on OSX  r59586@macbookpro (orig r28891):  michael | 2009-10-16 09:41:19 -0400  Bug: n/a  Description: changelog update  r59587@macbookpro (orig r28892):  andy | 2009-10-16 14:54:16 -0400  Bump to Audio::Scan 0.43  r59588@macbookpro (orig r28893):  andy | 2009-10-16 15:57:32 -0400  Audio::Scan 0.43, increase max buffer size to handle larger cover art  r59589@macbookpro (orig r28894):  andy | 2009-10-16 16:00:19 -0400  Add wavpack build script  r59590@macbookpro (orig r28895):  andy | 2009-10-16 16:02:43 -0400  Add strip section to the build script  r59591@macbookpro (orig r28896):  andy | 2009-10-16 16:31:54 -0400  Fix build on FreeBSD  r59592@macbookpro (orig r28897):  andy | 2009-10-16 16:33:27 -0400  Add wvunpack binaries for ARM and FreeBSD
    git-svn-id: file:///home/awy/mirror/slim/7.5/trunk/vendor@28899 62299810-d8cb-41fd-93b7-d32162e5a4a4

---
+++ wavpack/build.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# $Id $
+
+OS=`uname`
+
+# Build dir
+BUILD=$PWD/build
+
+FLAGS=""
+# Mac-specific flags (must be built on Leopard)
+if [ $OS = "Darwin" ]; then
+    FLAGS="-arch i386 -arch ppc -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.3"
+elif [ $OS = "FreeBSD" ]; then
+    # needed to find iconv
+    FLAGS="-I/usr/local/include -L/usr/local/lib"
+fi
+
+# FreeBSD's make sucks
+if [ $OS = "FreeBSD" ]; then
+    if [ !-x /usr/local/bin/gmake ]; then
+        echo "ERROR: Please install GNU make (gmake)"
+        exit
+    fi
+    export GNUMAKE=/usr/local/bin/gmake
+    export MAKE=/usr/local/bin/gmake
+else
+    export MAKE=/usr/bin/make
+fi
+
+# Clean up
+# XXX command-line flag to skip cleanup
+rm -rf $BUILD
+
+mkdir $BUILD
+
+# Build wavpack
+tar jxvf wavpack-4.50.1.tar.bz2
+cd wavpack-4.50.1
+CFLAGS="$FLAGS" \
+LDFLAGS="$FLAGS" \
+    ./configure --prefix=$BUILD \
+    --disable-dependency-tracking \
+    --disable-shared
+make
+if [ $? != 0 ]; then
+    echo "make failed"
+    exit $?
+fi
+make install
+cd ..
+rm -rf wavpack-4.50.1
+
+cp $BUILD/bin/wvunpack .
+rm -rf $BUILD
+
+if [ $OS = 'Darwin' ]; then
+    strip -S wvunpack
+elif [ $OS = 'Linux' -o $OS = "FreeBSD" ]; then
+    strip wvunpack
+fi
