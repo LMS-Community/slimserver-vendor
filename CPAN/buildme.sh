befc22453d300034d553ae5f77760979bff422fe
Andy Grundman <agrundman@slimdevices.com>
2009-11-21 04:21:46 +0000

    Bump Audio::Scan to 0.52
    git-svn-id: file:///home/awy/mirror/slim/7.4/trunk/vendor@29389 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- CPAN/buildme.sh
+++ CPAN/buildme.sh
@@ -238,7 +238,7 @@ function build {
             ;;
         
         Audio::Scan)
-            build_module Audio-Scan-0.51
+            build_module Audio-Scan-0.52
             ;;
         
         Template)
