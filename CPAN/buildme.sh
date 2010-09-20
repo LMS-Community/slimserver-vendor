4a46cf7dfb01047301b55feb2b77eff3ec694b93
Andy Grundman <agrundman@logitech.com>
2010-09-20 22:42:54 +0000

    Bump Image::Scale to 0.06
    git-svn-id: file:///home/awy/mirror/slim/7.6/trunk/vendor@31362 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- CPAN/buildme.sh
+++ CPAN/buildme.sh
@@ -488,8 +488,8 @@ function build {
             build_module Test-NoWarnings-1.02
             RUN_TESTS=1
             
-            tar zxvf Image-Scale-0.05.tar.gz
-            cd Image-Scale-0.05
+            tar zxvf Image-Scale-0.06.tar.gz
+            cd Image-Scale-0.06
             if [ $PERL_58 ]; then
                 # Running 5.8
                 $PERL_58 Makefile.PL --with-jpeg-includes="$BUILD/include" --with-jpeg-static \
@@ -541,7 +541,7 @@ function build {
             fi
             cd ..
             
-            rm -rf Image-Scale-0.05
+            rm -rf Image-Scale-0.06
             rm -rf giflib-4.1.6
             rm -rf libpng-1.4.3
             rm -rf jpeg-8b
