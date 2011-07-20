833226d89b1d63cf9a4b6a9fb42aefc0498c3e17
Andy Grundman <agrundman@logitech.com>
2011-07-20 19:51:38 +0000

    Audio::Scan 0.88 for 7.5 too
    git-svn-id: file:///home/awy/mirror/slim/7.5/trunk/vendor@32780 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- CPAN/buildme.sh
+++ CPAN/buildme.sh
@@ -377,7 +377,7 @@ function build {
             build_module Tree-DAG_Node-1.06
             build_module Test-Warn-0.23
             RUN_TESTS=1
-            build_module Audio-Scan-0.87
+            build_module Audio-Scan-0.88
             ;;
         
         Template)
