4634cf06afc68009c2833f88776e03bb77463180
Andy Grundman <agrundman@logitech.com>
2011-09-09 18:36:20 +0000

    Bump to Audio::Scan 0.92
    git-svn-id: file:///home/awy/mirror/slim/7.7/trunk/vendor@33398 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- CPAN/buildme.sh
+++ CPAN/buildme.sh
@@ -655,7 +655,7 @@ function build {
             build_module Tree-DAG_Node-1.06
             build_module Test-Warn-0.23
             RUN_TESTS=1
-            build_module Audio-Scan-0.91
+            build_module Audio-Scan-0.92
             ;;
 
         MP3::Cut::Gapless)
