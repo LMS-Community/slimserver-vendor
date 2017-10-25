LMS-Community/slimserver-vendor

commit 1559a0350556fbcad0cf7a31317ab750f7add990

BUGFIX: Correct /etc/make.conf parsing on FreeBSD.

wavpack/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 #
 # $Id $
 

flac/buildme-freebsd.sh
@@ -8,11 +8,11 @@ CHANGENO=`git rev-parse --short HEAD`
 OUTPUT=$PWD/flac-build-$ARCH-$CHANGENO
 
 if [ -f "/etc/make.conf" ]; then
-    CC=`grep CC /etc/make.conf | grep -v CCACHE | sed 's#CC=##g'`
-    CXX=`grep CXX /etc/make.conf | grep -v CCACHE | sed 's#CXX=##g'`
+    CC=`grep CC /etc/make.conf | grep -v CCACHE | grep -v \# | sed 's#CC=##g'`
+    CXX=`grep CXX /etc/make.conf | grep -v CCACHE | grep -v \# | sed 's#CXX=##g'`
 fi
 
-if [ -z $CC ]; then
+if [ -z CC ]; then
     CC=cc
 fi
 

shine-fixed/buildme-freebsd.sh
@@ -11,8 +11,8 @@ rm -rf $OUTPUT
 gmake -f Makefile.freebsd clean
 
 if [ -f "/etc/make.conf" ]; then
-    CC=`grep CC /etc/make.conf | grep -v CCACHE | sed 's#CC=##g'`
-    CXX=`grep CXX /etc/make.conf | grep -v CCACHE | sed 's#CXX=##g'`
+    CC=`grep CC /etc/make.conf | grep -v CCACHE | grep -v \# | sed 's#CC=##g'`
+    CXX=`grep CXX /etc/make.conf | grep -v CCACHE | grep -v \# | sed 's#CXX=##g'`
 fi
 
 if [ -z $CC ]; then

sox/buildme-freebsd.sh
@@ -12,8 +12,8 @@ ARCH=`uname -m`
 OUTPUT=$PWD/sox-build-$ARCH-$CHANGENO
 
 if [ -z ${CC} ] && [ -f "/etc/make.conf" ]; then
-    CC=`grep CC /etc/make.conf | grep -v CCACHE | sed 's#CC=##g'`
-    CXX=`grep CXX /etc/make.conf | grep -v CCACHE | sed 's#CXX=##g'`
+    CC=`grep CC /etc/make.conf | grep -v CCACHE | grep -v \# | sed 's#CC=##g'`
+    CXX=`grep CXX /etc/make.conf | grep -v CCACHE | grep -v \# | sed 's#CXX=##g'`
 fi
 
 if [ -z $CC ]; then

commit d31817023f7333a00577a96b70628649c0091333

BUGFIX: Correct shebang regression introduced in 1559a03.

wavpack/build.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/usr/bin/env bash
 #
 # $Id $
 