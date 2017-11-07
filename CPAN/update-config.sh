0ca6e48753bb98a4f908fc79c615b3daee09cd70
Charles Razzell <crazell@gmail.com>
2017-11-06 20:34:45 -0800

    fix incorrect test expression syntax in update-config.sh

--- CPAN/update-config.sh
+++ CPAN/update-config.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ "`uname -m`"=="aarch64" -a "`uname`"=="Linux"  ]; then
+if [ "`uname -m`" = "aarch64" -a "`uname`" = "Linux"  ]; then
     [ -f /tmp/config.guess.$$ ] || wget -O /tmp/config.guess.$$ 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
     [ -f /tmp/config.sub.$$ ] || wget -O /tmp/config.sub.$$ 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
     cp -vf /tmp/config.guess.$$ ./config.guess
